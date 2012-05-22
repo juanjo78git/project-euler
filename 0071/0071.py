62bc930df91a0b86a187fb16bc890edaa18ba62b
Pedro Nicolás <png1981@gmail.com>
2012-05-22 22:23:58 +0200

    Resuelto 0071, en menos de 2seg de tiempo en pypy

--- 0071/0071.py
+++ 0071/0071.py
@@ -1,4 +1,19 @@
-#!/usr/bin/pypy
+#!/usr/bin/python
+
+#Consider the fraction, n/d, where n and d are positive integers. If nd and 
+#HCF(n,d)=1, it is called a reduced proper fraction.
+
+#If we list the set of reduced proper fractions for d  8 in ascending order of 
+#size, we get:
+
+#1/8, 1/7, 1/6, 1/5, 1/4, 2/7, 1/3, 3/8, 2/5, 3/7, 1/2, 4/7, 3/5, 5/8, 2/3, 
+#5/7, 3/4, 4/5, 5/6, 6/7, 7/8
+
+#It can be seen that 2/5 is the fraction immediately to the left of 3/7.
+
+#By listing the set of reduced proper fractions for d  1,000,000 in ascending
+#order of size, find the numerator of the fraction immediately to the left 
+#of 3/7.
 
 def mcd(a, b):
 	if b == 0:
@@ -21,23 +36,21 @@ n_ant = 1
 limite = 1000000
 
 # n/d
-for d in range(limite, 1, -1):
+for d in range(1, limite+1):
 
-	if d % 1000 == 0:
-		print("d", d)
+	n_calc, m = divmod(n_fix*d, d_fix)
+	
+	if m == 0:
+		continue
+	
+	for n in range(n_calc, 0, -1):
 
-	for n in range(1, d):
-		if n % 100000 == 0:
-			print("n y d",n, d)
+		if mcd_it(n, d) == 1:
 			
-		# una vez es mayor, debemos romper
-		if (n*7) > (d*3):
-			break
-		
-		if mcd_it(n, d):
-			# quiero ver si el número es menor que 3/7...
-			if (n*7) < (d*3):
-				if (n*d_ant) > (d*n_ant):
-					d_ant = d
-					n_ant = n
-print(d, n)
+			if (n*d_ant) > (d*n_ant):
+				d_ant = d
+				n_ant = n
+			else:
+				break
+
+print("Resultado 0071", n_ant)
