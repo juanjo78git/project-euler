649ac58d1bb36563039cfeb1891c984af72c347e
Pedro Nicolás <png1981@gmail.com>
2012-06-10 22:50:28 +0200

    Planteado

--- 0061/0061.py
+++ 0061/0061.py
@@ -18,9 +18,81 @@ def heptagonal(n):
 def octagonal(n):
 	return int(n*(3*n-2))
 
+# joder, le paso la función como parámetro y listo, la uso como fn(n)
+def lxnal(fn):
+	l = []
+	n = 1
+	slen = 0
+	while slen < 5:
+		t = fn(n)
+		slen = len(str(t))	
+		if slen == 4:
+			l.append(t)
+		n += 1
+	return l	
+
+# prefijo serían ya los dos dos primeros números
+def tiene_prefijo(pre, l):
+	for x in l:
+		if pre == str(x)[0:2]:
+			return True
+	return False
+	
+def tiene_sufijo(suf, l):
+	for i in l:
+		if suf == str(i)[2:4]:
+			return True
+	return False
+
+def busca_pre_suf(n, l):
+	#print(n, i, l)
+	
+	pre = str(n)[0:2]
+	suf = str(n)[2:4]
+	bpre = False
+	bsuf = False
+	
+	for lx in l:
+		if tiene_prefijo(pre, lx):
+			bpre = True
+		
+		if tiene_sufijo(suf, lx):
+			bsuf = True
+
+	return bpre and bsuf
 
 
-for n in range(1, 10):
-	print(triangle(n))
+np = 6		 
+l = []
 
+# increiblemente facil
+l.append(lxnal(triangle))
+l.append(lxnal(square))
+l.append(lxnal(pentagonal))
+l.append(lxnal(hexagonal))
+l.append(lxnal(heptagonal))
+l.append(lxnal(octagonal))
+
+for lx in l:
+	print(len(lx))
+	#print(lx)
+
+
+
+li = 0
+lcopy = list(l)
+print(id(lcopy), id(l))
+for lx in l:
+	lminus = list(l)	
+	lminus.pop(li)
+	for i in lx:
+		if not busca_pre_suf(i, lminus):
+			lcopy[li].remove(i)
+			
+	li += 1	
+		
+
+for lx in lcopy:
+	print(len(lx))
+	#print(lx)	
 
---
+++ 0081/0081.py
@@ -0,0 +1,119 @@
+#!/usr/bin/pypy
+
+#Project Euler 0081
+
+#In the 5 by 5 matrix below, the minimal path sum from the top left to the 
+#bottom right, by only moving to the right and down, is indicated in bold red 
+#and is equal to 2427.
+
+
+#131	673	234	103	18
+#201	96	342	965	150
+#630	803	746	422	111
+#537	699	497	121	956
+#805	732	524	37	331
+
+#Find the minimal path sum, in matrix.txt (right click and 'Save Link/
+#Target As...'), a 31K text file containing a 80 by 80 matrix, from the top 
+#left to the bottom right by only moving right and down.
+
+# eliminando (erroneamente) rango, me sale 391499
+
+# ady: matriz de adyacencias
+# nd: número de nodos
+def floyd_warshall(ady, nn):
+	path = ady
+	
+	for i in range(0, nn):
+		path[i][i] = 0
+
+	for k in range(0, nn):
+		print('fw:', k)
+		for i in range(0, nn):
+			for j in range(0, nn):	
+				dt = path[i][k] + path[k][j]
+				if path[i][j] > dt:
+					path[i][j] = dt
+	return path
+
+
+def fich_to_mat(fichero, dim):
+	m = [[0 for col in range(dim)] for row in range(dim)]
+	f = open(fichero, 'r')
+
+	x = 0
+	for linea in f:	
+		y = 0
+		for i in linea.split(','):
+			m[x][y] = float(i)
+			y += 1
+		x += 1
+	f.close()
+	
+	return m
+	
+# la matriz, la dimension de la matriz	
+def mat_to_ady(m, dim):	
+	nn = dim*dim
+	ady = [[0 for col in range(nn)] for row in range(nn)]
+	
+	for i in range(0, nn):
+		for j in range(0, nn):
+			ady[i][j] = float('inf')
+	
+	for i in range(0, dim):
+		for j in range(0, dim):
+			
+			ady_orig = (i*dim)+j
+			#print(i, j, m[i][j], ady_orig)
+			
+			
+			#print(m[0][1])
+			
+			#print('@@@')
+						
+			# derecha
+			if j < dim-1:
+				ady_der = (i*dim)+j+1
+				ady[ady_orig][ady_der] = m[i][j+1]
+				#print(i, j, m[i][j+1], ady_orig, ady_der)
+
+				
+			if i < dim-1:
+				ady_abaj = ((i+1)*dim)+j
+				ady[ady_orig][ady_abaj] = m[i+1][j]			
+				#print(i, j, m[i+1][j], ady_orig, ady_abaj)
+	
+			#print('###')
+			
+			#printmat(ady, dim*dim)
+	
+	return ady
+
+
+def printmat(m, dim):
+	for i in range(0, dim):
+		print(m[i])
+	print(' ')
+	
+
+dim = 80
+fichero = './matrix.txt'
+
+m = fich_to_mat(fichero, dim)
+ady = mat_to_ady(m, dim)
+p = floyd_warshall(ady, dim*dim)
+
+
+print('Resultado 0081:', p[0][0] + p[0][79])
+
+
+
+
+
+
+
+
+
+
+
