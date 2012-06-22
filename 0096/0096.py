176d48d30fd8ea7ecba1a22b55a9a125f8540291
Pedro Nicolás <pedro.nicolas.gomez@everis.com>
2012-06-22 13:28:21 +0200

    Métodos añadidos:
    Se añaden los primeros métodos para devolver la fila, columna y cubo
    además se añaden dos métodos más para determinar si un sudoku es
    válido y otro para ver si es solución

--- 0096/0096.py
+++ 0096/0096.py
@@ -30,12 +30,14 @@ class Sudoku:
 		s += "+-----+-----+-----+"
 		return s	
 
-	""" devuelve la fila n """
+	
 	def getfila(self, n):
+		""" devuelve la fila n """
 		return self.sudoku[(9*n):((9*n)+9)]
 
-	""" mira que número falta en el cubo n """
+
 	def getmissnumcube(self, n):
+		""" mira que numero falta en el cubo n """
 		miss = [1,2,3,4,5,6,7,8,9]
 		
 		cube = self.getcube(n)
@@ -43,44 +45,67 @@ class Sudoku:
 			if cube[i] != 0:
 				miss.remove(int(cube[i]))
 	
-	""" le paso una columna de la 0 a la 8 y me devuelve los datos... """ 
+	
 	def getcolumn(self, n):
+		""" le paso una columna de la 0 a la 8 y me devuelve los datos... """ 
 		column = []
 		for j in range(9):
 			column.append(self.sudoku[n+(j*9)])
 		
 		return column
 
-	""" le paso una fila de la 0 a la 8 y me devuelve los datos... """ 
-	def getcubo(self, n):
+	def getcube(self, n):
+		""" le paso una fila de la 0 a la 8 y me devuelve los datos... """ 
 		cubo = []
-		for c in range(3)
-			rango = 
-			cubo += self.sudoku
+		for f in range(3):
+			for i in range(3):
+				e = (27 * (n / 3)) + (f * 9) + i + ((n % 3) * 3)
+				cubo.append(self.sudoku[e])
+		return cubo
 
-
-
-# vamos a ver como lo puedo montar
-
-# le pasamos una matriz 3x3 y nos devuelve una lista con los numeros que del
-# 1 al 9 faltan
-def get_list_num_faltan_m3(matriz):
-	
-	faltan = [1,2,3,4,5,6,7,8,9]
-
-	#for i in range(3):
-	#	for j in range(3):
-	#		if m3[i][j] != 0:
-	#			faltan.remove(m3[i][j])
-
-
-	for fila in matriz:
-		for i in fila:
+	def is_valid_partial(self, partial):
+		""" si una linea, cubo y columna es valida """
+		complet = [1,2,3,4,5,6,7,8,9]
+		for i in partial:
 			if i != 0:
-				faltan.remove(i)
+				if i not in complet:
+					return False
+				else:
+					complet.remove(i)
+		return True
+		
+	def is_valid(self):
+		""" si es valido, no si es solucion """
+		for i in range(9):
+			if not self.is_valid_partial(self.getcube(i)):
+				return False
+			if not self.is_valid_partial(self.getcolumn(i)):
+				return False
+			if not self.is_valid_partial(self.getfila(i)):
+				return False
+		return True
+				
 
-	return faltan
-	
+	def is_solved_partial(self, partial):
+		if [1,2,3,4,5,6,7,8,9] == partial.sort():
+			return True
+		else:
+			False
+		
+	def is_solved(self):
+		""" si es solucion """
+		for i in range(9):
+			if not self.is_solved_partial(self.getcube(i)):
+				return False
+			if not self.is_solved_partial(self.getcolumn(i)):
+				return False
+			if not self.is_solved_partial(self.getfila(i)):
+				return False
+		return True
+		
+		
+				
+# vamos a ver como lo puedo montar
 
 # notas: con el mod, podemos quedarnos con con el minicuadrado de 3x3:
 #
@@ -89,15 +114,23 @@ def get_list_num_faltan_m3(matriz):
 a = ([[1,2,3], [4,5,0], [0,7,8]])
 
 
-f = get_list_num_faltan_m3(a)
 
 
-sudoku = Sudoku("023456789123456789223456789123456789123456789123456789123456789123456789123456789")
+
+#sudoku = Sudoku("023456789123456789223456789123456789123456789123456789123456789123456789777888999")
+
+sudoku = Sudoku("300200000000107000706030500070009080900020004010800050009040301000702000000008006")
 
 print(sudoku)
 
 
+print(sudoku.getcube(0))
+print(sudoku.getcube(1))
 print(sudoku.getcube(2))
+print(sudoku.getcube(3))
+print(sudoku.getcube(8))
+print(sudoku.is_valid())
+print(sudoku.is_solved())
 #print(sudoku.getcolumn(8))
 
 
