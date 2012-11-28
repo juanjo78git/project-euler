f6692e6844ec21309aadec940b3b0d542df47d52
Pedro Nicolás <png1981@gmail.com>
2012-11-28 10:47:17 +0100

    Tabulado de 0015.c

--- 0015/0015.c
+++ 0015/0015.c
@@ -1,4 +1,4 @@
-# include "stdio.h"
+#include <stdio.h>
 
 
 
@@ -29,46 +29,48 @@
 #define LEN_CAMINO		8
 */
 
-
+/* Cabeceras */
 void mult_matrix(long a[][NUM_NODOS], long b[][NUM_NODOS], long r[][NUM_NODOS]);
 void matrizcpy(long orig[][NUM_NODOS], long des[][NUM_NODOS]);
 void prlong_matrix(long a[][NUM_NODOS]);
+void elevar_matriz(long m[][NUM_NODOS], long potencia);
 
+/* funciones */
 void elevar_matriz(long m[][NUM_NODOS], long potencia)
 {
-	long i;
-	long aux[NUM_NODOS][NUM_NODOS];
-	long resul[NUM_NODOS][NUM_NODOS];
-	
-	if (potencia < 2)
-	{
-		return;
-	}
-	
-	mult_matrix(m, m, resul);
-	
-	for (i = 2; i < potencia; i++)
-	{
-		mult_matrix(m, resul, resul);
-	}
-	
-	matrizcpy(resul, m);
+    long i;
+    long aux[NUM_NODOS][NUM_NODOS];
+    long resul[NUM_NODOS][NUM_NODOS];
+
+    if (potencia < 2)
+    {
+        return;
+    }
+
+    mult_matrix(m, m, resul);
+
+    for (i = 2; i < potencia; i++)
+    {
+        mult_matrix(m, resul, resul);
+    }
+
+    matrizcpy(resul, m);
 }	
 
 
 void matrizcpy(long orig[][NUM_NODOS], long des[][NUM_NODOS])
 {
-	long i;
-	long j;
+    long i;
+    long j;
 
-	
-	for (i = 0; i < NUM_NODOS; i++)
-	{
-		for (j = 0; j < NUM_NODOS; j++)
-		{
-			des[i][j] = orig[i][j];
-		}
-	}
+
+    for (i = 0; i < NUM_NODOS; i++)
+    {
+        for (j = 0; j < NUM_NODOS; j++)
+        {
+            des[i][j] = orig[i][j];
+        }
+    }
 }
 
 
@@ -77,10 +79,10 @@ void prlong_matrix(long a[][NUM_NODOS])
 {
     long i, j;
     for (i = 0; i < NUM_NODOS; i++) {
-	for (j = 0; j < NUM_NODOS; j++) {
-	    printf("%d\t", a[i][j]);
-	}
-	printf("\n");
+        for (j = 0; j < NUM_NODOS; j++) {
+            printf("%d\t", a[i][j]);
+        }
+        printf("\n");
     }
 
     printf("\n\n");
@@ -90,27 +92,25 @@ void prlong_matrix2(long a[][NUM_NODOS_N])
 {
     long i, j;
     for (i = 0; i < NUM_NODOS_N; i++) {
-	for (j = 0; j < NUM_NODOS_N; j++) {
-	    printf("%d\t", a[i][j]);
-	}
-	printf("\n");
+        for (j = 0; j < NUM_NODOS_N; j++) {
+            printf("%d\t", a[i][j]);
+        }
+        printf("\n");
     }
 
     printf("\n\n");
 }
 
-
-void
-mult_matrix(long a[][NUM_NODOS], long b[][NUM_NODOS], long r[][NUM_NODOS])
+void mult_matrix(long a[][NUM_NODOS], long b[][NUM_NODOS], long r[][NUM_NODOS])
 {
     long i, j, k;
 
     for (i = 0; i < NUM_NODOS; i++) {
-	for (j = 0; j < NUM_NODOS; j++) {
-	    r[i][j] = 0;
-	    for (k = 0; k < NUM_NODOS; k++)
-		r[i][j] += a[i][k] * b[k][j];
-	}
+        for (j = 0; j < NUM_NODOS; j++) {
+            r[i][j] = 0;
+            for (k = 0; k < NUM_NODOS; k++)
+                r[i][j] += a[i][k] * b[k][j];
+        }
     }
 }
 
@@ -127,52 +127,47 @@ void calcula_matriz_adyacencia(long a[][NUM_NODOS])
     //long
 
     for (i = 0, cont = 0; i < NUM_NODOS_N; i++) {
-	for (j = 0; j < NUM_NODOS_N; j++) {
-	    aux[i][j] = cont++;
-	}
+        for (j = 0; j < NUM_NODOS_N; j++) {
+            aux[i][j] = cont++;
+        }
 
     }
 
-    prlong_matrix2(aux);
+    /*prlong_matrix2(aux);*/
 
     for (i = 0, cont = 0; i < NUM_NODOS_N; i++) {
-	for (j = 0; j < NUM_NODOS_N; j++) {
+        for (j = 0; j < NUM_NODOS_N; j++) {
 
-	    // vemos cuales son los que vamos a tratar
-	    x = aux[i][j];
+            // vemos cuales son los que vamos a tratar
+            x = aux[i][j];
 
-	    //printf("valores de i e j [%d][%d]\n", i, j);
+            //printf("valores de i e j [%d][%d]\n", i, j);
 
-	    if (j < NUM_NODOS_N - 1) {
-		j_mas_1 = aux[i][j + 1];
-		a[x][j_mas_1] = 1;
+            if (j < NUM_NODOS_N - 1) {
+                j_mas_1 = aux[i][j + 1];
+                a[x][j_mas_1] = 1;
 
-		//printf("no estamos en borde por j, x[%d], j_mas_1[%d]\n",
-		       //x, j_mas_1);
-	    }
+                //printf("no estamos en borde por j, x[%d], j_mas_1[%d]\n",
+                //x, j_mas_1);
+            }
 
-	    if (i < NUM_NODOS_N - 1) {
-		i_mas_1 = aux[i + 1][j];
-		a[x][i_mas_1] = 1;
-		//printf("no estamos en borde por i, x[%d], i_mas_1[%d]\n",
-		      // x, j_mas_1);
-	    }
+            if (i < NUM_NODOS_N - 1) {
+                i_mas_1 = aux[i + 1][j];
+                a[x][i_mas_1] = 1;
+                //printf("no estamos en borde por i, x[%d], i_mas_1[%d]\n",
+                // x, j_mas_1);
+            }
 
-
-	    //prlong_matrix(a);
-
-	}
-
+            //prlong_matrix(a);
+        }
     }
 
     //prlong_matrix(a);
-
 }
 
 
 
-
-main()
+int main(void)
 {
     //long mult[NUM_NODOS][NUM_NODOS];
 
@@ -181,82 +176,82 @@ main()
 
     long i, j;
     for (i = 0; i < NUM_NODOS; i++) {
-	for (j = 0; j < NUM_NODOS; j++) {
-	    m1[i][j] = 0;
-	    //m2[i][j] = 0;
-	}
+        for (j = 0; j < NUM_NODOS; j++) {
+            m1[i][j] = 0;
+            //m2[i][j] = 0;
+        }
     }
 
     calcula_matriz_adyacencia(m1);
 
     //calcula_matriz_adyacencia(m2);
 
-	//prlong_matrix(m1);
+    /*prlong_matrix(m1);*/
 
-/*
-        printf("Multiplication of the Matrices:\n");
-        for(i=0;i<r1;i++)
-        {
-            for(j=0;j<c2;j++)
-            {
-                mult[i][j]=0;
-                for(k=0;k<r1;k++)
-                    mult[i][j]+=m1[i][k]*m2[k][j];
-            }
-        }
-*/
-
-
-	/* se elevará al número k dependiendo del número del camino */
+    /*
+       printf("Multiplication of the Matrices:\n");
+       for(i=0;i<r1;i++)
+       {
+       for(j=0;j<c2;j++)
+       {
+       mult[i][j]=0;
+       for(k=0;k<r1;k++)
+       mult[i][j]+=m1[i][k]*m2[k][j];
+       }
+       }
+       */
 
 
-
-	elevar_matriz(m1, LEN_CAMINO);
-
-	//prlong_matrix(m1);
-	
-	printf("Número de caminos: [%ld]\n", m1[0][NUM_NODOS-1]);
-	
-	return 0;
+    /* se elevará al número k dependiendo del número del camino */
 
 
 
-	
-	/* la primera multiplicacion para realizar el M² */
-/*	mult_matrix(m1, m2, mult);
-	
-	for (i = 1; i < LEN_CAMINO - 1; i++)
-	{
-		mult_matrix(m1, mult, mult);
-	
-	
-	}
+    elevar_matriz(m1, LEN_CAMINO);
 
-    
+    //prlong_matrix(m1);
 
-    prlong_matrix(mult);
-
-    mult_matrix(m1, mult, mult);
-
-
-    prlong_matrix(mult);
-
-    
-
-    prlong_matrix(mult);
-
-    mult_matrix(m1, mult, mult);
-
-    prlong_matrix(mult);
-
-    mult_matrix(m1, mult, mult);
-
-    prlong_matrix(mult);
-
+    printf("Número de caminos: [%ld]\n", m1[0][NUM_NODOS-1]);
 
     return 0;
-    
-    */
+
+
+
+
+    /* la primera multiplicacion para realizar el M² */
+    /*	mult_matrix(m1, m2, mult);
+
+        for (i = 1; i < LEN_CAMINO - 1; i++)
+        {
+        mult_matrix(m1, mult, mult);
+
+
+        }
+
+
+
+        prlong_matrix(mult);
+
+        mult_matrix(m1, mult, mult);
+
+
+        prlong_matrix(mult);
+
+
+
+        prlong_matrix(mult);
+
+        mult_matrix(m1, mult, mult);
+
+        prlong_matrix(mult);
+
+        mult_matrix(m1, mult, mult);
+
+        prlong_matrix(mult);
+
+
+        return 0;
+
+*/
 }
 
 
