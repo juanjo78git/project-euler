4e64991c31c7635410a7ed769793304ff8573d3a
Pedro Nicolás <png1981@gmail.com>
2012-11-28 09:52:39 +0100

    Añadidos los problemas 5, 6 y 7
    Para el 7, además he generado el iterador de números primos, que queda super
    bonito.

---
+++ 0006/0006.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+
+limite = 100
+sumacuadrados = 0
+cuadradossuma = 0
+
+for i in range(1, limite + 1):
+    sumacuadrados += i**2
+    cuadradossuma += i
+
+resta = cuadradossuma**2 - sumacuadrados
+
+
+print ("Resultado 0006", resta)
--- lib/mymaths.py
+++ lib/mymaths.py
@@ -1,8 +1,50 @@
 # mymaths algunas funciones que uso mucho
 
 def ispalindrome(n):
+    """ Retorna si un numero es palindromo """
     s = str(n)
     for i in range(0, len(s)):
         if s[i] != s[len(s)-i-1]:
             return False
     return True
+
+
+def isprime(n):
+    """ Retorna si un numero es primo """
+    if n == 1:
+        return False
+
+    # rango empieza en 2, y solo tenemos que llegar hasta el cuadrado de n
+    for x in range(2, int(n**0.5)+1):
+        if n % x == 0:
+            return False
+
+    return True
+
+
+
+
+
+
+
+
+# GENERADORES __________________________________________________________________
+
+
+def fibonacci():
+    """ Generador de la secuencia Fibonacci"""
+    a, b = 0, 1
+    while True:
+        yield a
+        a, b = b, a + b
+
+def prime():
+    """ Generador de secuencia numeros primos"""
+    p = 2
+    while True:
+        yield p
+        
+        while (not isprime(p + 1)):
+            p += 1
+
+        p = p + 1
