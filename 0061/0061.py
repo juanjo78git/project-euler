037621b209655701ff1c12a08762e0e852d3fb7c
Pedro Nicolás <png1981@gmail.com>
2012-12-01 11:18:19 +0100

    Solución a 0061:
    Ayer tenía una solución parcial, he limpiado esa solución y terminado de
    completarla. ahora está lista.
    
    Nos olvidamos de este horrible problema.

--- 0061/0061.py
+++ 0061/0061.py
@@ -1,14 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-
-# idea para limpiar
-#
-# no lo metas en una lista general, ve sacando los datos de las
-# listas individuales y luego vas viendo que listas has usado
-# y cuales no. joder, como el del foro... o intenta dejar esta 
-# solución lo más simple posible
-
 import itertools
 
 def triangle(n):
@@ -48,104 +40,65 @@ def lxnal(fn):
     return l
 
 
-def esciclico_quick(l, ndc):
-    ''' Retorna si una lista es ciclica en ndc factores por digito, pero esta
-    vez muy rapida, el metodo anterior era un puto desastre '''
-    lpre = []
-    lsuf = []
 
-    for li in l:
-        lpre.append(str(li)[ndc:])
-        lsuf.append(str(li)[0:ndc])
 
-    lpre.sort()
-    lsuf.sort()
-    if set(lpre) == set(lsuf):
-        # puede haber falsos positivos
-        return esciclico_ordenado(l, ndc)
+def cumple(l, solucion):
+    laux = list(l)
+    solaux = [] 
+    for s in solucion:
+        for l in laux:
+            if s in l:
+                laux.remove(l)
+                if s not in solaux:
+                    solaux.append(s)    
+
+    if len(laux) == 0 and len(solaux) == 6:
+        return True
     else:
         return False
 
-def esciclico(l, ndc):
-    ''' Retorna si es ciclico, a partir de un listado
-    l: listado, ndc: numero de digitos ciclicos '''
-    n = len(l)
-
-    for i in range(0, n):
-        j = i
-        k = (i + 1) % n
-        # final con inicio
-        if str(l[j])[ndc:] != str(l[k])[0:ndc]:
-            return False
-    return True
-
-
-def esciclico_ordenado(l, ndc):
-    ''' A partir de un listado, nos dice si es ciclico cualquiera de las
-    permutaciones pusibles '''
-    for lp in itertools.permutations(l):
-        if esciclico(lp, ndc):
-            return True
-
-    return False
-
-
-
-def lsufijos(elemento, l):
-    lsuf = []
-    for i in l:
-        if str(elemento)[2:] == str(i)[0:2]:
-            lsuf.append(i)
-
-    return lsuf
-
-
-def edl(lista, lsolucion):
-    for i in lsolucion:
-        if i in lista:
-            return True
-
-    return False
-
-def estadiflistas(ltotal, lsolucion):
-    for li in ltotal:
-        if not edl(li, lsolucion):
-            return False
-
-    return True
         
 
 
+def siguientes(e, l):
+    sig = []
+    for i in l:
+        if e % 100 == i / 100:
+            sig.append(i)
 
-def ryp(ltotal, l, lsolucion):
+    return sig
 
-    if len(lsolucion) == 6:
-        if esciclico_quick(lsolucion, 2):
-            if estadiflistas(ltotal, lsolucion):
-                lsolucion.sort()
-                print "Solucion Final:", lsolucion, sum(lsolucion)
+def recursolucion(l, lunida, solucion):
+
+    if len(solucion) == 6:
+        if solucion[0] / 100 ==  solucion[5] % 100:
+            if cumple(l, solucion):
+                print "Resultado 0061:", solucion, sum(solucion)
         return
 
     # sacamos el listado de sufijos
-    ultimo = lsolucion[len(lsolucion)-1]
-    lsuf = lsufijos(ultimo, l)
-    #print "Lista sufijos", ultimo, lsuf
+    ultimo = solucion[len(solucion)-1]
+    sig = siguientes(ultimo, lunida)
+    #print "Lista sufijos", ultimo, sig
 
-    for i in lsuf:
-        lcopy = list(l)
-        lcopy.remove(i)
-        lcopysolucion = list(lsolucion)
-        lcopysolucion.append(i)
+    for i in sig:
+        lunidaaux = list(lunida)
+        lunidaaux.remove(i)
+        solucionaux= list(solucion)
+        solucionaux.append(i)
         #print "Solucion parcial:", lcopysolucion
-        ryp(ltotal, lcopy, lcopysolucion)
+        recursolucion(l, lunidaaux, solucionaux)
 
-    
-
+def gensolucion(l, lunida):
+    for i in l[0]:
+        lunidaaux = list(lunida)
+        lunidaaux.remove(i)
+        solucion = []
+        solucion.append(i)
+        recursolucion(l, lunidaaux, solucion)
 
 
 # variables
-numero_digitos_ciclicos = 2
-np = 6
 l = []
 
 # increiblemente facil
@@ -156,74 +109,11 @@ l.append(lxnal(hexagonal))
 l.append(lxnal(heptagonal))
 l.append(lxnal(octagonal))
 
-ltotal = []
+lunida = []
 for lx in l:
     for i in lx:
-        if i not in ltotal:
-            ltotal.append(i)
+        if i not in lunida:
+            lunida.append(i)
 
-ltotal.sort()
-
-for i in ltotal:
-    laux = list(ltotal)
-    laux.remove(i)
-    lsol = []
-    lsol.append(i)
-    ryp(l, laux, lsol)
-
-
-
-count = 0
-total = 0
-# vamos a ver, voy a intentar ejecutar esto...
-
-#for i in range(0, 6):
-    #print len(l[i])
-
-
-
-#for n3 in l[0]:
-    #for n4 in l[1]:
-        #for n5 in l[2]:
-            #for n6 in l[3]:
-                #for n7 in l[4]:
-                    #for n8 in l[5]:
-                        #laux = []
-                        #laux.append(n3)
-                        #laux.append(n4)
-                        #laux.append(n5)
-                        #laux.append(n6)
-                        #laux.append(n7)
-                        #laux.append(n8)
-                        #count += 1
-                        #total += 1
-                        #if count == 1000000:
-                            #count = 0
-                            #print total
-
-                        #if esciclico_quick(laux, 2):
-                            #print("Resultado provisional:", laux)
-                            #lresult = list(laux)
-
-
-#print("Resultado 0061:", sum(lresult))
-
-
-
-#li = 0
-#lcopy = list(l)
-#print(id(lcopy), id(l))
-#for lx in l:
-    #lminus = list(l)
-    #lminus.pop(li)
-    #for i in lx:
-        #if not busca_pre_suf(i, lminus):
-            #lcopy[li].remove(i)
-
-    #li += 1
-
-
-#for lx in lcopy:
-    #print(len(lx))
-    ##print(lx)
+gensolucion(l, lunida)
 
