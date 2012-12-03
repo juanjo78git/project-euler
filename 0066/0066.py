0b652d22ec1e9e21c4ec0a15fc441ebaf3695c4f
Pedro Nicolás <png1981@gmail.com>
2012-12-03 11:33:14 +0100

    Resuelto 0066:
    Tras revisar como son las ecuaciones Pell, y descubrir que lo trabajado en el
    ejercicio 0064 me serviría y de mucho, saco una solución rápida.
    
    Añado también el 0066_bruto.py para demostrar que una aproximación bruta
    como fue mi primer impulso es un desastre.

--- 0066/0066.py
+++ 0066/0066.py
@@ -1,37 +1,100 @@
-#!/usr/bin/pypy
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
 
-def is_square(n):
-	square = n**0.5
-	if int(square) == square:
-		return True
-	else:
-		return False
+#Por ejemplo, √19 tiene la expansión en fracciones continuas
+#que es recurrente cada 6 fracciones. El término inmediatamente anterior al punto
+#en el que se repite es 170/39 y la teoría de Lagrange dice que
 
+#x = 39, y = 170
 
-def diophantine(d, y2):	
-	x = ((y2*d)+1)**0.5
-	if x == int(x):
-		return int(x)
-	else:
-		return -1
-	
+#será la solución más pequeña a la ecuación de Pell
 
-D_result = 0
-x_min = -1
-limite = 1000
-for D in range(1, limite+1):
-	y = 1
-	if not is_square(D):
-		dio = False
-		while not dio: 
-			x = diophantine(D, (y*y))
-			y += 1
-			if x != -1:
-				dio = True
-				print(D, x)
-				if x > x_min:
-					x_min = x
-					D_result = D
+#19x2 + 1 = y2.
 
-print(D_result, x_min)
+#http://hojaynumeros.blogspot.com.es/2010/02/ecuacion-de-pell.html
 
+def contfrac(r, a, b):
+    ''' solo para el calculo de las fracciones '''
+    # ejemplo, el anterior paso se había quedado así
+    # 1 + (raiz(23) - 3 / 7)
+    # i = 1
+    # a = 3
+    # b = 7
+    #
+    # ahora quiero para el siguiente paso la fraccion:
+    # 7 / raiz(23) - 3
+    # b / raiz(23) - a
+    newi = int(b / ((r ** 0.5) - a))
+    newb = r - (a * a) 
+    # b es divisor de newb
+    newb = newb / b
+    z = newi * newb
+    newa = a - z
+    #print r, newi, newa, newb
+    return newi, (newa * -1), newb
+
+def fracxy(i, xy0, xy1):
+    return (i * xy1) + xy0
+
+def espell(x, y, d):
+    if (x**2) - (d*(y**2)) == 1:
+        return True
+    else:
+        return False
+
+def calcfrac(r):
+    if r ** 0.5 == int(r ** 0.5):
+        return -1
+    i = int(r ** 0.5)
+    x0 = 0
+    x1 = 1
+    y0 = 1
+    y1 = 0
+
+    # primera fraccion...
+    x, y = fracxy(i, x0, x1), fracxy(i, y0, y1)
+    x0 = x1
+    x1 = x
+    y0 = y1
+    y1 = y
+
+    # primera llamada
+    i, a, b = contfrac(r, int(r ** 0.5), 1)
+
+    x, y = fracxy(i, x0, x1), fracxy(i, y0, y1)
+    x0 = x1
+    x1 = x
+    y0 = y1
+    y1 = y
+
+    # si es pell, osea, solución
+    if espell(x, y, r):
+        return x
+
+    count = 0
+    while True:
+        i, a, b = contfrac(r, a, b)
+        x, y = fracxy(i, x0, x1), fracxy(i, y0, y1)
+        
+        x0 = x1
+        x1 = x
+        y0 = y1
+        y1 = y
+    
+        if espell(x, y, r):
+            break
+    return x
+
+LIMITE = 1000
+
+maxx= 0
+res = 0
+
+for i in range(1, LIMITE + 1):
+    x = calcfrac(i)
+    
+    if x > maxx:
+        maxx = x
+        res = i
+
+print("Resultado 0062:", res)
