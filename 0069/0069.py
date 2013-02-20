efe1b12efaaef4fea8e5c47b77968262d057afa3
Pedro Nicolás <png1981@gmail.com>
2013-02-21 00:01:16 +0100

    0069 resuelto

--- 0069/0069.py
+++ 0069/0069.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/pypy
 
 import os, sys
 lib_path = os.path.abspath('../lib')
@@ -15,8 +15,7 @@ def genlprimes(n):
     prime = p.next()
     # quiero una lista de primos que dividan a n, por lo tanto con
     # buscar solo la mitad me va bien
-    limit = (n * 0.5) + 1
-    print limit
+    limit = n + 1
     while prime < limit:
         l.append(prime)
         prime = p.next() 
@@ -24,21 +23,55 @@ def genlprimes(n):
     return l
 
 def numdivsprimes(lprimes, n):
-    """ lista de primos que son divisibles por el numero """
+    """ lista de primos que son divisibles por el numero n, pasandole la lista
+        de primos """
     ldiv = []
+
+    if n in lprimes:
+        ldiv.append(n)
+        return ldiv
+
     np = 0
     d = lprimes[np]
     while n != 1:
-       r, m = divmod(n, d)
-       if m == 0:
-           n = r
-           if d not in ldiv:
-               ldiv.append(d)
-       else:
-           d = lprimes[np]
+        r, m = divmod(n, d)
+        if m == 0:
+            n = r
+            if d not in ldiv:
+                ldiv.append(d)
+        else:
+            np += 1
+            d = lprimes[np]
 
     return ldiv
 
+def phi(lprimes, n):
+    """ funcion phi, a partir de un listado de primos, para facilitar """
 
-l =  genlprimes(LIMIT)
-print numdivsprimes(l, 999999231)
+    ldivs = numdivsprimes(lprimes, n)
+    arriba = 1
+    abajo = 1
+
+    for div in ldivs:
+        arriba = (div - 1) * arriba
+        abajo = div * abajo
+
+    return (n * arriba) / abajo
+
+
+n_max = 0
+n_div_phi_max = 0
+lprimes =  genlprimes(LIMIT)
+
+for n in range(2, LIMIT):
+    n_phi = phi(lprimes, n)
+    n_div_phi_aux = n/float(n_phi)
+
+    if n_div_phi_aux > n_div_phi_max:
+            n_div_phi_max = n_div_phi_aux
+            n_max = n
+
+print "Resultado 0069: ", n_max
+
+
+
