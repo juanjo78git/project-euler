5c90d2286bf163e6c06299b08fb41154dd99f2c4
Pedro Nicolás <png1981@gmail.com>
2013-04-17 18:25:25 +0200

    Todavía no funciona, pero apaño cosas

--- 0093/0093.py
+++ 0093/0093.py
@@ -2,24 +2,117 @@
 # -*- coding: utf-8 -*-
 
 import itertools
+import operator
+
+# añadimos la propiedad asociativa, así generaremos muchas menos combinaciones,
+# por ejemplo para el caso de + + + automáticamente se calculará como
+# a b c d + + +
+# de igual forma para + y - y para multiplicaciones.
+
+def add_resultados(l, r):
+    if r not in l and r > 0:
+        l.append(r)
+    return l
+
+def resultados(a, b, c, d, op1, op2, op3):
+    ''' entramos sin no asociativa... '''
+    res = []
+
+    r = eval('a op1 b op3 c op3 d')  
+    l = add_resultados(l, r)
+
+    r = eval('(a op1 b) op3 c op3 d')  
+    l = add_resultados(l, r)
+
+    r = eval('a op1 (b op3 c) op3 d')  
+    l = add_resultados(l, r)
+
+    r = eval('a op1 b op3 (c op3 d)')  
+    l = add_resultados(l, r)
+
+    r = eval('(a op1 b op3 c) op3 d')  
+    l = add_resultados(l, r)
+
+    r = eval('a op1 (b op3 c op3 d)')  
+    l = add_resultados(l, r)
+
+    r = eval('(a op1 b) op3 (c op3 d)')  
+    l = add_resultados(l, r)
+
+    r = eval('((a op1 b) op3 c) op3 d')  
+    l = add_resultados(l, r)
+
+    r = eval('a op1 (b op3 (c op3 d))')  
+    l = add_resultados(l, r)
+
+    return l
 
 
+
+
+def asociativa(op):
+    if op[0] in ('-','+') and op[1] in ('+', '-') and op[2] in ('+', '-'):
+        return True
+    else:
+        if op[0] in ('*') and op[1] in ('*') and op[2] in ('*'):
+            return True
+
+    return False
+
+
+def rpnvalid(rpn):
+   
+    if not rpn[0].isdigit():
+        return False
+        
+    if rpn[6].isdigit():
+        return False
+
+    if not rpn[1].isdigit() and not rpn[2].isdigit():
+        return False
+
+    if rpn[3].isdigit() and rpn[4].isdigit() and rpn[5].isdigit():
+        return False
+
+    return True
 
 def p93(a, b, c, d):
     pos_rpns = []
     # para cada union de 3 operadores...
+    #element = str(a) + str(b) + str(c) + str(d)
     for op in list(itertools.combinations_with_replacement('+-*/', 3)):
-        elements = str(a) + str(b) + str(c) + str(d) + "".join(op)
-        for pos_rpn in list(itertools.permutations(elements, 7)):
-            #if pos_rpn[0] >= '0' and pos_rpn[0] <= '9':
-            res = ''
-            for i in pos_rpn:
-                res = res + i + ' '
-            pos_rpns.append(res)
+        
+        elem = [a, b, c, d, op[0], op[1], op[2]]
+        
+        if asociativa(op):
+            #pos_rpn2.append(op[0])
+            #pos_rpn2.append(op[1])
+            #pos_rpn2.append(op[2])
+            pos_rpns.append(elem)
+        else:
+            for per in list(itertools.permutations('0123456', 7)):
+                rpn = []
+                for i in per:
+                    # a ver, i sería la convinación 0123456
+                    rpn.append(str(elem[int(i)]))
+
+                if rpnvalid(rpn):
+                    pos_rpns.append(rpn)
     
     return pos_rpns
 
 
+def p93_normal(a, b, c, d):
+    pos_rpns = []
+    # para cada union de 3 operadores...
+    element = str(a) + str(b) + str(c) + str(d)
+    for op in list(itertools.combinations_with_replacement('+-*/', 3)):
+        #elements = element + "".join(op)
+        for pos_rpn in list(itertools.permutations(elements, 4)):
+                if rpnvalid(pos_rpn):
+                    pos_rpns.append(pos_rpn)
+    
+    return pos_rpns
 
 # rpn calculadora...
 operators = {
@@ -42,28 +135,49 @@ def polish_eval(expression):
             pile.append(int(e))
     return pile[0]
 
+ARITHMETIC_OPERATORS = {
+    '+':  operator.add, '-':  operator.sub,
+    '*':  operator.mul, '/':  operator.div, 
+}
 
-MAX = 120
+def postfix(expression, operators=ARITHMETIC_OPERATORS):
+    stack = []
+    #for val in expression.split():
+    for val in expression:
+        if val in operators:
+            f = operators[val]
+            stack[-2:] = [f(*stack[-2:])]
+        else:
+            stack.append(int(val))
+    return stack.pop()
+
+
+
+MAX = 25
+INIT = 1
 
 def p93_3():
     maximo = 0
-    for a in range(1, MAX):
+    for a in range(INIT, MAX):
         print a
         for b in range(a + 1, MAX):
+            print b
             for c in range(b + 1, MAX):
                 for d in range(c + 1, MAX):
+                    l = []
                     l = p93(a, b, c, d)
                     
                     r = []
 
                     for i in l:
                         try:
-                            p = polish_eval(i)
+                            p = postfix(i)
                             if p > 0:
                                 if p not in r:
                                     r.append(p)
                         except:
                             z = 0
+                            #print i
 
                     # una vez llegados aqui tenemos una lista de los valores
                     # que hemos ido obteniendo, la pintamos de primeras...
