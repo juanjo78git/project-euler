c28e1f8286c586c4e924b0e987176423d0a61478
Pedro Nicolás <png1981@gmail.com>
2013-05-31 10:14:17 +0200

    Solucion a 0119
    Otra solución que tarda 0.14 segundos, la idea no es ir buscando de uno en uno
    los números que cumplen, la idea es calcular el s^e y luego ver si el
    resultado es una suma de s.
    
    En ese caso lo metemos en una lista, luego la ordenamos y devolvemos el valor
    del registro 30.

--- 0119/0119.py
+++ 0119/0119.py
@@ -5,8 +5,11 @@
 from datetime import datetime
 
 TH = 30
-L = 10000
-N = 10000
+#L = 10000
+#N = 10000
+
+N = 100
+E = 100
 
 powers = []
 
@@ -17,61 +20,21 @@ def sumdigits(n):
         sumatory += int(i)
     return sumatory
 
-
-def is_0119(n):
-    s = sumdigits(n)
-    if s == 1:
-        return False
-    exp = 1
-
-    # para ganar velocidad, vamos a ir dando saltos con el exponente hasta
-    # dar con un número que sea mayor, luego restaremos este valor y así
-    # saltamos muchos
-
-    #while True:
-        #exp += 2
-        #power = s ** exp
-        #if power > n:
-            #exp -= 2
-            #break
-
-    while True:
-        pos_power = powers[s][exp]
-        if pos_power is None:
-            power = s ** exp
-            powers[s][exp] = power
-        else:
-            power = pos_power
-
-        if power == n:
-            print "%d = %d^%d" % (n, s, exp)
-            return True
-        elif power > n:
-            return False
-        exp += 1
-
-
-def gen_default_powers(L, N):
-    powers = range(0, L)
-    for i in range(0, L):
-        powers[i] = [None] * N
+def gen_pos_result(N, E):
+    powers = []
+    for i in range(2, N):
+        for j in range(2, E):
+            power = i ** j
+            if sumdigits(power) == i:
+                powers.append(power)
     return powers
 
 
 # controlamos el tiempo de ejecución
 start_time = datetime.now()
 
-powers = gen_default_powers(L, N)
-
-n = 10
-i = 0
-while True:
-    if is_0119(n):
-        i += 1
-        print i
-        if i == TH:
-            break
-    n += 1
+powers = gen_pos_result(N, E)
+powers.sort()
 
 print "Tiempo total: ", datetime.now() - start_time
-print "Resultado de 0123 ", n
+print "Resultado de 0123: ", powers[TH - 1]
