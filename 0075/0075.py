77b0985611106f321a557a31fe6d038c438d1dfa
Pedro Nicolás <png1981@gmail.com>
2013-05-31 14:55:20 +0200

    Para trabajar en casa

--- 0075/0075.py
+++ 0075/0075.py
@@ -10,9 +10,19 @@ lib_path = os.path.abspath('../lib')
 sys.path.append(lib_path)
 
 import mymaths
+import math
+
+
+LIMITE = 1000000
 
 # controlamos el tiempo de ejecución
 start_time = datetime.now()
+
+for a in range(1, LIMITE):
+    for b in range(1, LIMITE):
+        c = math.sqrt((a**2) + (b**2))
+        if c == int(c):
+            print a, b, c
 
 print "Tiempo total: ", datetime.now() - start_time
 print "Resultado de 0075: "
