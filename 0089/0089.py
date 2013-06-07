a2c45444a43ce4868f3d7b80efb66bcaa6c53d49
penicolas <png1981@gmail.com>
2013-06-07 12:16:26 +0200

    Solución más completa a 0089
    Tal como había indicado, 0089 estaba solventado, la primera parte por mi y la
    segunda parte tirando de la librería de python "roman", esta necesidad ha sido
    eliminado y ahora la solución es 100% mia.

--- 0089/0089.py
+++ 0089/0089.py
@@ -2,8 +2,8 @@
 
 #!/usr/bin/python
 
-import roman
 from datetime import datetime
+
 
 class Roman:
     """ Número romano... """
@@ -11,11 +11,17 @@ class Roman:
         self._romval = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500,
                         'M': 1000}
 
+        self._romvalinv = {v: k for k, v in self._romval.items()}
+
         # reglas de resta
-        self._lawsub = {'I': ['V', 'C'], 'X': ['L', 'C'], 'C': ['D', 'M']}
+        self._lawsub = {'I': ['V', 'X'], 'X': ['L', 'C'], 'C': ['D', 'M']}
+
+        self._values = {900: 'CM', 400: 'CD', 90: 'XC', 40: 'XL', 9: 'IX',
+                        4: 'IV'}
 
         self._romano = romano
-        self._numero = 0
+        self._numero = self._calc_numero()
+        self._rommin = romano
 
     def get_number(self):
         return self._numero
@@ -56,7 +62,72 @@ class Roman:
 
         return total
 
-# controlamos el tiempo de ejecución
+    def _calc_roman(self):
+        """ calcula el número romano a partir del número """
+
+        n = self._numero
+        roman = []
+
+        # primero los miles, que no es posible agruparlos
+        while (n >= 1000):
+            roman.append(self._romvalinv[1000])
+            n -= 1000
+
+        # intentamos centenas
+        if n >= 900:
+            roman.append(self._values[900])
+            n -= 900
+
+        if n > 499:
+            roman.append(self._romvalinv[500])
+            n -= 500
+
+        if n > 399:
+            roman.append(self._values[400])
+            n -= 400
+
+        while n >= 100:
+            roman.append(self._romvalinv[100])
+            n -= 100
+
+        # intentamos las decenas
+        if n >= 90:
+            roman.append(self._values[90])
+            n -= 90
+
+        if n > 49:
+            roman.append(self._romvalinv[50])
+            n -= 50
+
+        if n > 39:
+            roman.append(self._values[40])
+            n -= 40
+
+        while n >= 10:
+            roman.append(self._romvalinv[10])
+            n -= 10
+
+        # intentamos las unidades
+        if n >= 9:
+            roman.append(self._values[9])
+            n -= 9
+
+        if n > 4:
+            roman.append(self._romvalinv[5])
+            n -= 5
+
+        if n > 3:
+            roman.append(self._values[4])
+            n -= 4
+
+        while n >= 1:
+            roman.append(self._romvalinv[1])
+            n -= 1
+
+        return ''.join(roman)
+
+
+# controlamor el tiempo de ejecución
 start_time = datetime.now()
 
 len_org = 0
@@ -68,7 +139,8 @@ for l in open('roman.txt'):
 
     len_org += len(s)
     n = r._calc_numero()
-    len_fin += len(roman.toRoman(n))
+    len_fin += len(r._calc_roman())
+
 
 print "Tiempo total: ", datetime.now() - start_time
 print "Resultado de 0089: ", len_org - len_fin
