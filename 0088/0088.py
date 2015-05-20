bdc4cfecfe48974c0d6d77ad89e21b9043b23093
Pedro Nicolás Gómez <png1981@gmail.com>
2015-05-20 20:09:41 +0200

    Solución a 0088

--- 0088/0088.py
+++ 0088/0088.py
@@ -22,47 +22,40 @@ def get_k(nums):
     # print "k: ", k, " -- valor sumatorio: ", product, "lista: ", nums
     return k
 
+# http://stackoverflow.com/questions/24723721/how-can-i-generate-all-possible-divisor-products-for-a-number 
+def products(n, min_divisor=2):
+    """Generate expressions of n as a product of ints >= min_divisor."""
+    if n == 1:
+        yield []
+    for divisor in range(min_divisor, n+1):
+        if n % divisor == 0:
+            for product in products(n // divisor, divisor):
+                yield product + [divisor]
 
-def res(k_max, limit):
+
+def res(k_max):
     result = {}
-    for n in range(4, limit):
+    for n in range(4, (k_max * 2) + 1):
 
         # tenemos que ir sacando todos los posibles divisores de n
         # if n % 1000 == 0:
         # s = sum(list(set(result.values())))
         # print n, len(result.keys()), s
-        m = n
-        for d1 in range(2, ((n / 2) + 1)):
-            divs = []
-            m = n
-            for d2 in range(d1, ((n / 2) + 1)):
-                while m % d2 == 0:
-                    m = m / d2
-                    if m > 1:
-                        # tenemos un divisor
-                        divs.append(d2)
-                        k = get_k(divs + [m])
-                        
-                        if k < k_max + 1:
-
-                            if k == 28:
-                                print divs + [m]
-
-
-                            if result.has_key(k):
-                                if result[k] > n:
-                                    result[k] = n
-                            else:
-                                result[k] = n
-
+        for product in products(n):
+            k = get_k(product)
+            if k < k_max + 1:
+                if result.has_key(k):
+                    if result[k] > n:
+                        result[k] = n
+                else:
+                    result[k] = n
+    
     s = sum(list(set(result.values())))
     # print sorted(result.items(), key=lambda x: x[1])
     # print result
     
     # for i in sorted(result.keys()):
     #     print i, ":", result[i]
-
-
     print s
     return s
 
@@ -70,8 +63,8 @@ def res(k_max, limit):
 # controlamor el tiempo de ejecución
 start_time = datetime.now()
 
-# s2 = res(12, 100)
-s2 = res(28, 60)
+# s2 = res(12)
+s2 = res(12000)
 
 print "Tiempo total: ", datetime.now() - start_time
 print "Resultado de 0088: ", s2
