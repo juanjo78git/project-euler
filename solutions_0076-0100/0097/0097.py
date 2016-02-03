0230534723922ef85221d7820ccd94b053bf6f13
Pedro Nicolás Gómez <png1981@gmail.com>
2016-02-03 15:14:48 +0100

    Solución a 0097
    Ya tenía resuelto el problema 0097, pero no lo tenía guardado, el motivo es
    que es muy fácil y quizás lo hice directamente en terminal.
    
    El caso es que si quieres únicamente los últimos 10 dígitos, pues guardas
    únicamente los 10 dígitos finales en tu cálculo de 2*2*2*2*...

---
+++ solutions_0076-0100/0097/0097.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+#!/usr/bin/python
+
+from datetime import datetime
+
+def solution_0097():
+
+    # primero calculamos los últimos 10 dígitos de 2^7830457
+    n = 2
+    for i in range(1, 7830457):
+        n = n * 2
+        n = int(str(n)[-10:])
+
+    return int(str((n * 28433) + 1)[-10:])
+
+
+start_time = datetime.now()
+
+resultado = solution_0097() 
+
+print "Tiempo total: ", datetime.now() - start_time
+
+print "Resultado de 0097: ", resultado
