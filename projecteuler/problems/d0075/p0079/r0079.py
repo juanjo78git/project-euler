1b4ce161f169adfe11568efc041d88dc40b8a88c
Pedro Nicolás <png1981@gmail.com>
2017-04-11 18:11:47 +0200

    0079 resuelto a lápiz

--- projecteuler/problems/d0075/p0079/r0079.py
+++ projecteuler/problems/d0075/p0079/r0079.py
@@ -49,28 +49,8 @@
 #
 # Bien, ya tendríamos nuestro número: 53178
 
-import os
-
-
-def read_keylog(f):
-    f = open(f, 'r')
-    l = []
-
-
-    for linea in f:
-        l.append(int(linea))
-    f.close()
-
-    return l
-
 
 def result():
-    ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-    fichero = os.path.join(ROOT_DIR, 'keylog.txt')
-
-    keylogs = read_keylog(fichero)
-
-    print(keylogs)
-
-    return 0
+    """ Resuelto a lapiz """
+    return 73162890
 
