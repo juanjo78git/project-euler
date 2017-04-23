253f87ad85047457a91247a5374ae2c50a5c9cb8
Pedro Nicolás Gómez <png1981@gmail.com>
2017-04-23 23:32:35 +0200

    Resuelto 205
    Tengo siempre un problema importante con las probabilidades: siempre
    pienso que serán sencillos y luego palmo una barbaridad de horas.
    
    En este caso el problema fue no ver que devuelve Python con sus
    `itertools.combinations_with_replacement()`: Devuelve lo que yo estaba
    buscando, que son todas las combinaciones de posibles resultados de los
    datos, pero NO cuantas veces se repite esa combinación. Un ejemplo más
    pequeño para comprenderlo:
    
    ```
    >>> list(itertools.combinations_with_replacement([1, 2], 2))
    [(1, 1), (1, 2), (2, 2)]
    ```
    Está claro que esas son las posibles combinaciones, pero la (1, 2) se
    repite 2 veces, mientras que las otras solo una vez.
    
    Sabiendo ésto, lo que hago es utilizar las combinaciones y luego cuando
    veo que se una posible solución mirar cuantas veces se da esa
    combinación con `itertools.permutations()` y listo.
    
    Solución en minuto y pico en Pypy3. Me parece aceptable.

---
+++ RACHAS.md
@@ -0,0 +1,16 @@
+# RACHAS
+
+## Abril 2017
+
+Last 10 Problems Solved
+
+| 1  | Problem 191 | 22 Apr 17 (12:43) |
+| 2  | Problem 173 | 22 Apr 17 (11:19) |
+| 3  | Problem 387 | 21 Apr 17 (13:38) |
+| 4  | Problem 113 | 20 Apr 17 (22:49) |
+| 5  | Problem 108 | 19 Apr 17 (17:26) |
+| 6  | Problem 103 | 16 Apr 17 (23:38) |
+| 7  | Problem 105 | 16 Apr 17 (22:37) |
+| 8  | Problem 101 | 16 Apr 17 (09:16) |
+| 9  | Problem 111 | 14 Apr 17 (17:05) |
+| 10 | Problem 134 | 12 Apr 17 (21:15) |
---
+++ projecteuler/problems/d9999/p0205/r0205.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+
+import itertools
+
+
+def count_perm(l, size):
+    return len(list(set((itertools.permutations(l, size)))))
+
+
+def prob_pyram_cubic(pyramidal_dice, cubic_dice):
+
+    cs = list(itertools.combinations_with_replacement([1, 2, 3, 4, 5, 6], cubic_dice))
+    ps = list(itertools.combinations_with_replacement([1, 2, 3, 4], pyramidal_dice))
+    
+    win_p = 0
+    t_p = 1
+    t_c = 1
+    now = False
+
+    for p in ps:
+        now = True
+        for c in cs:
+            if sum(p) > sum(c):
+                # LLLLLLLLLLLLLLLLLLegados a este punto, queremos saber cuantas
+                # de esas combinaciones se pueden dar, y para eso usamos
+                # permutaciones
+                if now:
+                    t_p = count_perm(p, pyramidal_dice)
+                now = False
+                t_c = count_perm(c, cubic_dice)
+
+                win_p += ((1 / (6 ** cubic_dice)) * (1 / (4 ** pyramidal_dice))) * t_p * t_c
+
+    return win_p
+
+
+def result():
+    num_pyramidal_dice = 9
+    num_cubic_dice = 6
+    p = prob_pyram_cubic(num_pyramidal_dice, num_cubic_dice)
+
+    # retornamos el valor como 0.abcdefg
+    return '%.7f' % p
