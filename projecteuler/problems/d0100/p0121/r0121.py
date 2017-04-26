a7ca25046c0de1fb621e46c0ce9ed44af8b9024e
Pedro Nicolás <png1981@gmail.com>
2017-04-26 13:47:16 +0200

    Resuelto 0121
    Ok, recursividad al trapo.
    
    Voy montando la solución parcial recursivamente añadiendo un disco rojo en cada
    paso y revisando si es una posible solución, así voy montando cadenas así:
    
                R
              R
            /   B
        - R
            \   R
              B
                B
    
                R
              R
            /   B
        - B
            \   R
              B
                B
    
    En el ejemplo, de tamaño 3, vemos que es posible solución serían las cadenas:
    
        - R B B --> 1/2 * 1/3 * 1/4
        - B R B --> 1/2 * 2/3 * 1/4  (nótese que cuando sale Red, hay dos discos)
        - B B R --> 1/2 * 1/3 * 3/4
        - B B B --> 1/2 * 1/3 * 1/4
    
    Sumamos todo, y tenemos
    
        Prob: 1/24 + 2/24 + 3/24 + 1/24 = 7/24
        Libras = 24/7 = 3
    
    Luego la idea es montar una función recursiva que en cada paso lance añadiendo
    un paso donde hemos recuperado una ficha azul, y otro paso donde hemos
    recuperado una ficha roja.

--- projecteuler/problems/d0100/p0121/r0121.py
+++ projecteuler/problems/d0100/p0121/r0121.py
@@ -20,29 +20,28 @@ def discgame(blues, reds, curr_blues, curr_reds, turn, turns, total, visual):
 
     if turn == turns:
         if curr_blues > curr_reds:
-            print(visual, total)
+            # print(visual, total)
             return total, den
         else:
             return 0, 0
 
     # jugamos y sacamos una ficha azul
-    t, d = discgame(blues, reds + 1, curr_blues + 1, curr_reds, turn + 1, turns, total, visual + ['A'])
+    t, d = discgame(blues, reds + 1, curr_blues + 1, curr_reds, turn + 1, 
+                    turns, total, visual + ['A'])
     parcial += t
     
     # jugamos y sacamos una ficha roja
-    t, d = discgame(blues, reds + 1, curr_blues, curr_reds + 1, turn + 1, turns, total + reds - 1, visual + ['R'])
+    t, d = discgame(blues, reds + 1, curr_blues, curr_reds + 1, turn + 1,
+                    turns, total * reds, visual + ['R'])
     parcial += t
 
     return parcial, den
 
 
 def result():
-    print('hola')
     
-    MAX_TURNS = 5
+    MAX_TURNS = 15
 
-    total, total2 = discgame(1, 1, 0, 0, 0, MAX_TURNS, 1, [])
-    print(total, total2)
-    print(divmod(total2, total // 2)[0])
-
-    return 0
+    prob, total = discgame(1, 1, 0, 0, 0, MAX_TURNS, 1, [])
+    # print(prob, total)
+    return divmod(total, prob)[0]
