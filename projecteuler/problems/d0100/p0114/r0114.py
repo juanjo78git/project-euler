8fb9588f49ad5c29a1223c8a130de2e2800d1892
Pedro Nicolás Gómez <png1981@gmail.com>
2017-04-29 12:38:16 +0200

    Resuelto 114
    Al igual que para los ejercicios 116 y 117, he montado una solución
    valida en recursivo, sabiendo que con un tamaño 50 no iba a terminar
    jamás.
    
    Pero con esa función recursiva puedo ver los valores que va montando la
    secuencia. Como en anteriores, es algo así como un fibonacci, pero en
    este caso se aplica una corrección a cada termino de [+1, +1, 0, -1,
    -1]:
    
     - f(n) = f(n-1) + f(n-2) + 1
     - f(n) = f(n-1) + f(n-2) + 1
     - f(n) = f(n-1) + f(n-2) + 0
     - f(n) = f(n-1) + f(n-2) - 1
     - f(n) = f(n-1) + f(n-2) + 0
    
    Un poco loco, la verdad, pero que tiene que ver si el bloque termina en
    un espacio o no termina en espacio.
    
    En programar 45 minutos, mientras de fondo sonaba *Lift Your Skinny
    Fists Like Antennas to Heaven* de **Godspeed You! Black Emperor**.  En
    ejecución menos de un segundo.

--- projecteuler/problems/d0100/p0114/r0114.py
+++ projecteuler/problems/d0100/p0114/r0114.py
@@ -1,6 +1,21 @@
 # -*- coding: utf-8 -*-
 
 
+def gen_114():
+    yield 1
+    yield 1
+    yield 1
+    a = 2
+    b = 1
+    offset = [1, 1, 0, -1, -1, 0]
+    i = 0
+    while True:
+        yield a
+        a, b = a + b + offset[i], a
+        i = (i + 1) % len(offset)
+        # print('offset', offset[i])
+
+
 def tiles_114(total, offset, min_size, total_blocks, was_last_space, solution):
 
     if offset == total_blocks:
@@ -32,16 +47,18 @@ def result():
     total = 0
 
     N_BLOCKS = 50
-    # N_BLOCKS = 7
     TILE_MINIM_SIZE = 3
     
+    g = gen_114()
     # total = 0
     # offset = 0
     # tamaño mínimo de azulejos
     # número de bloque
     # que era lo último que le hemos pasado
     for n_b in range(0, N_BLOCKS + 1):
-        total = tiles_114(0, 0, TILE_MINIM_SIZE, n_b, True, [])
-        print(n_b, total)
+        # recursivo UNICAMENTE para ver como es la secuencia y estudiarla
+        # total = tiles_114(0, 0, TILE_MINIM_SIZE, n_b, True, [])
+        # print(n_b, total, next(g))
+        solucion = next(g)
 
-    return total
+    return solucion
